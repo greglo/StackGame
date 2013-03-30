68c611cca985ff41e0703e8a55f1727da0a90221
Michael Savage <mikejsavage@gmail.com>
2013-03-30 00:58:17 +0000

    remove old mike machine

--- mike/Value.java
+++
@@ -1,9 +0,0 @@
-public abstract class Value {
-	public abstract int asInt();
-
-	public abstract Value ADD( Value rhs );
-	public abstract Value SUB( Value rhs );
-
-	public abstract Value add( IValue rhs );
-	public abstract Value sub( IValue rhs );
-}
