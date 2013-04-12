634963ee283174402d91729048b36e105bc70818
Dan Fox <iamdanfox@gmail.com>
2013-04-12 14:51:24 +0100

    ApplicationFrame for UI

---
+++ src/ox/stackgame/ui/ApplicationFrame.java
@@ -0,0 +1,38 @@
+package ox.stackgame.ui;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+import javax.swing.border.*;
+
+
+
+public class ApplicationFrame {
+	
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("Stack Game");
+		frame.setBounds(200, 100, 1000, 760);
+		frame.setResizable(false);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		// window contents
+		JPanel contentPane = new JPanel(new BorderLayout());
+		Color codeacademyBlue = new Color(41,56,61);
+		contentPane.setBackground(codeacademyBlue);
+		frame.setContentPane(contentPane);
+		
+		// fake ControllerComponent
+		JPanel cc = new JPanel();
+		cc.setBackground(Color.white);
+		cc.setPreferredSize(new Dimension(250,100));
+		contentPane.add(cc, BorderLayout.WEST);
+		
+		frame.setVisible(true);
+	}
+
+}
