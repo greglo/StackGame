19e01a6375c43263e10b8c77e6a57f32524716e8
Dan Fox <iamdanfox@gmail.com>
2013-04-12 12:24:06 +0100

    Abstract Challenge class
    Abstract class is the most general representation, also includes a
    simpler, more practical subclass.

---
+++ src/ox/stackgame/challenge/AbstractChallenge.java
@@ -0,0 +1,47 @@
+/**
+ * 
+ */
+package ox.stackgame.challenge;
+
+import java.util.Collection;
+import java.util.HashSet;
+
+import ox.stackgame.stackmachine.*;
+import ox.stackgame.stackmachine.instructions.Instruction;
+
+/**
+ * @author danfox
+ * A representation of the concept of a 'challenge', involving instructions, success conditions etc.
+ */
+public abstract class AbstractChallenge {
+	public final String description;
+    public final int stackSize; // =200
+    public final int maxInstructions; // = 10000;
+    public final int storeSize;// = 4;
+    public final Collection<Class<? extends Instruction>> allowedInstructions; 
+	
+    /**
+     * Create a new challenge.
+     * @param description Plaintext string explaining to the user what the aim is (e.g. Divide [expr] by [expr] with a stack size of x.)  Could include hints.
+     * @param allowedInstructions A collection of allowed types of instruction for this challenge.  Send null to allow all instructions.
+     * @param stackSize
+     * @param maxInstructions
+     * @param storeSize
+     */
+	AbstractChallenge (String description, Collection<Class<? extends Instruction>> allowedInstructions, 
+			int stackSize, int maxInstructions, int storeSize){
+		this.description = description;
+		this.allowedInstructions = allowedInstructions;
+		// TODO add a constuctor to StackMachine that allows these to be set.
+		this.stackSize = stackSize;
+		this.maxInstructions = maxInstructions;
+		this.storeSize = storeSize;
+	}
+	
+	/**
+	 * Determine whether the supplied machine meets the success criteria for the challenge.
+	 * @param m a StackMachine to test.
+	 * @return "" if the machine has passed, a message otherwise.  Note, the failure message could include a helpful tip.
+	 */
+	public abstract String hasSucceeded(StackMachine m);
+}
---
+++ src/ox/stackgame/challenge/StackResultChallenge.java
@@ -0,0 +1,37 @@
+package ox.stackgame.challenge;
+
+import java.util.HashSet;
+
+import ox.stackgame.stackmachine.StackMachine;
+import ox.stackgame.stackmachine.StackMachine.EvaluationStack;
+import ox.stackgame.stackmachine.instructions.Instruction;
+
+/**
+ * A simple type of challenge that simply presents a problem and then allows the user to use any tools at their
+ * disposal to solve it, basing success only on whether they leave the Stack in the correct state. (NB this checks
+ * the entire stack, not just the top value - possibly should be changed).
+ * @author danfox
+ *
+ */
+public class StackResultChallenge extends AbstractChallenge {
+	
+	private final EvaluationStack correctAnswer;
+	
+	public StackResultChallenge(String description, EvaluationStack correctAnswer) {				
+		super(description, new HashSet<Class<? extends Instruction>> (), StackMachine.STACK_SIZE, StackMachine.MAX_INSTRUCTIONS, StackMachine.STORE_SIZE);
+		this.correctAnswer = correctAnswer;
+	}
+
+	@Override
+	public String hasSucceeded(StackMachine m) {
+		// Compare just top value:
+		//if (m.getStack().peek().equals(correctAnswer.peek())) {
+		// Compare entire stack:
+		if (m.getStack().equals(correctAnswer)){
+			return "";
+		} else {
+			return "Not quite, try again";
+		}
+	}
+
+}
