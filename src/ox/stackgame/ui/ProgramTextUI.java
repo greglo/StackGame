06d14b968fd9cdd6f7fc12d1cd0061b9eeab0fb2
Dan Fox <iamdanfox@gmail.com>
2013-04-13 16:30:12 +0100

    ProgramUI now has lines numbers, scrolls nicely.  Pretty colours.

--- src/ox/stackgame/ui/ProgramTextUI.java
+++ src/ox/stackgame/ui/ProgramTextUI.java
@@ -3,15 +3,16 @@
  */
 package ox.stackgame.ui;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Container;
-import java.awt.Dimension;
+import java.awt.*;
 
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import javax.swing.event.*;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Caret;
+import javax.swing.text.DefaultHighlighter;
 import javax.swing.text.Element;
+import javax.swing.text.Highlighter;
 
 import ox.stackgame.stackmachine.*;
 
@@ -25,6 +26,8 @@ import ox.stackgame.stackmachine.*;
  */
 public class ProgramTextUI extends JPanel {
 	private StackMachine activeMachine = null;
+	private Highlighter highlighter;
+	final JTextArea jta;
 	
 	private ModeVisitor modeActivationVisitor = new ModeVisitor(){
 		public void visit(Mode m) {}	
@@ -57,9 +60,22 @@ public class ProgramTextUI extends JPanel {
 
 	private StackMachineListener l = new StackMachineListenerAdapter() { 
 		public void programCounterChanged(int line) {
-			// TODO Change highlighting based on pc
+			try {
+				highlighter.removeAllHighlights();
+				highlighter.addHighlight(jta.getLineStartOffset(line), jta.getLineEndOffset(line), new DefaultHighlighter.DefaultHighlightPainter(Color.cyan));
+			} catch (BadLocationException e){
+				throw new RuntimeException ("pc shouldn't be out of bounds");
+			}
+
 		}
 	};
+	
+	// I think this is unnecessary
+//	private class MyHighlighter extends DefaultHighlighter.DefaultHighlightPainter {
+//		MyHighlighter(Color c) {
+//			super(c);
+//		}
+//	}
 	
 	public ProgramTextUI(ModeManager m){
 		super(new BorderLayout());
@@ -78,42 +94,62 @@ public class ProgramTextUI extends JPanel {
 		jsp.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
 		jsp.getViewport().setBackground(Color.red);
 		//jsp.getViewport().setBackground(ApplicationFrame.codeacademyBlue);
-		final JTextArea jta = new JTextArea();
+		
+		Font f = new Font(Font.MONOSPACED, Font.PLAIN, 15);
+		Color caRed = new Color(186,96,96);
+		
+		jta = new JTextArea();
 		jta.setBackground(ApplicationFrame.codeacademyBlue);
-		jta.setForeground(Color.red);
+		jta.setForeground(caRed);
+		jta.setMargin(new Insets(20,20,100,20));
+		//jta.setBorder(new EmptyBorder (20,20,20,20));
+		jta.setFont(f);
+		jta.setCaretColor(new Color(150,150,150));
+//		new Color(55,59,65)
+		
 		final JTextArea lines = new JTextArea("1");
+		//lines.setBorder(new EmptyBorder (20,20,20,0));
+		lines.setMargin(new Insets(20,20,100,0));
+		lines.setForeground(new Color(150,150,150));
+		lines.setBackground(ApplicationFrame.codeacademyBlue);
+		lines.setFont(f);
+		lines.setAlignmentX(RIGHT_ALIGNMENT);
 		
-		lines.setBackground(Color.LIGHT_GRAY);
+
 		lines.setEditable(false);
+
+		highlighter = jta.getHighlighter();		
+		
+		// listen for changes in jta and update lines.
+		jta.getDocument().addDocumentListener(new DocumentListener(){
+			public String getLinesText(){
+				int caretPosition = jta.getDocument().getLength();
+				Element root = jta.getDocument().getDefaultRootElement();
+				String text = "1" + System.getProperty("line.separator");
+				for(int i = 2; i < root.getElementIndex( caretPosition ) + 2; i++){
+					text += i + System.getProperty("line.separator");
+				}
+				return text;
+			}
+
+			public void changedUpdate(DocumentEvent de) {
+				lines.setText(getLinesText());
+			}
+ 
+			public void insertUpdate(DocumentEvent de) {
+				lines.setText(getLinesText());
+			} 
+
+			public void removeUpdate(DocumentEvent de) {
+				lines.setText(getLinesText());
+			} 
+		});
 		
 		
-//		jta.getDocument().addDocumentListener(new DocumentListener(){
-//			public String getText(){
-//				int caretPosition = jta.getDocument().getLength();
-//				Element root = jta.getDocument().getDefaultRootElement();
-//				String text = "1" + System.getProperty("line.separator");
-//				for(int i = 2; i < root.getElementIndex( caretPosition ) + 2; i++){
-//					text += i + System.getProperty("line.separator");
-//				}
-//				return text;
-//			}
-//
-//			public void changedUpdate(DocumentEvent de) {
-//				lines.setText(getText());
-//			}
-// 
-//			public void insertUpdate(DocumentEvent de) {
-//				lines.setText(getText());
-//			} 
-//
-//			public void removeUpdate(DocumentEvent de) {
-//				lines.setText(getText());
-//			} 
-//		});
 		
 		jsp.getViewport().add(jta);
 		//jsp.getViewport().setComponentZOrder(jta, 0);
-//		jsp.setRowHeaderView(lines);
+		jsp.setRowHeaderView(lines);
 		
 		// TODO sort out overlap problem. JTextArea is being painted above it's parent z order.
 		// http://stackoverflow.com/questions/10093425/java-how-to-draw-non-scrolling-overlay-over-scrollpane-viewport
