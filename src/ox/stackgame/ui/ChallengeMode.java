aed90928eba375d2e7c6321c110a004bffed0219
Ryan <ryan.gossiaux@merton.ox.ac.uk>
2013-04-21 15:36:14 +0100

    Add current challenge to ChallengeMode

--- src/ox/stackgame/ui/ChallengeMode.java
+++ src/ox/stackgame/ui/ChallengeMode.java
@@ -3,12 +3,27 @@
  */
 package ox.stackgame.ui;
 
+import ox.stackgame.challenge.AbstractChallenge;
+
 /**
  * Allows the user to create a program to solve a specific challenge (limited instruction set, 
- * well defined success conditions, specified max stack size)
+ * well defined success conditions, specified max stack size).
+ * 
+ * At the moment, it stores the current challenge. 
+ * 
  * @author danfox
+ * @author rgossiaux
  * 
  */
 public class ChallengeMode extends DesignMode {
-	// TODO store current challenge
+	protected AbstractChallenge challenge;
+	
+	public ChallengeMode() {
+		challenge = null;
+	}
+	
+	public ChallengeMode(AbstractChallenge challenge) {
+		this.challenge = challenge;
+	}
+	
 }
