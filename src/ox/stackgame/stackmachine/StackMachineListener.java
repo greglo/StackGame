6c79ac219c7d4436e83361bd46d3f67caac699d1
Greg O'Connor <gregory@oconnor.name>
2013-04-21 16:07:31 +0100

    Add stackInstructionsChanged to StackMachineListener.java

--- src/ox/stackgame/stackmachine/StackMachineListener.java
+++ src/ox/stackgame/stackmachine/StackMachineListener.java
@@ -1,6 +1,11 @@
 package ox.stackgame.stackmachine;
 
+import java.util.List;
+
+import ox.stackgame.stackmachine.instructions.Instruction;
+
 public interface StackMachineListener {
+    public void stackInstructionsChanged(List<Instruction> instructions);
     public void programCounterChanged(int line);
     public void storeChanged(int address);
     public void inputConsumed(int startIndex);
