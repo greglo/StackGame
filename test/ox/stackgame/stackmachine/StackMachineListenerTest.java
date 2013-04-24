b5983309b4bf2abc00213154c5479bcd3d4c22a6
Dan Fox <iamdanfox@gmail.com>
2013-04-24 14:55:43 +0100

    Listener Test Framework
    One test implemented, four to go.

---
+++ test/ox/stackgame/stackmachine/StackMachineListenerTest.java
@@ -0,0 +1,69 @@
+package ox.stackgame.stackmachine;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import ox.stackgame.stackmachine.instructions.Instruction;
+
+public class StackMachineListenerTest {
+
+	class MockListener implements StackMachineListener{
+		public boolean stackInstructionsChangedFired = false;
+		public void stackInstructionsChanged(List<Instruction> instructions) {
+			this.stackInstructionsChangedFired = true;
+		}
+
+		public boolean stackProgramCounterChanged = false;
+		public void programCounterChanged(int line) {
+			this.stackProgramCounterChanged = true;
+		}
+
+		public boolean storeChangedFired = false;
+		public void storeChanged(int address) {
+			this.storeChangedFired=true;
+		}
+
+		public boolean inputConsumedFired=false;
+		public void inputConsumed(int startIndex) {
+			this.inputConsumedFired=true;
+		}
+
+		public boolean outputChangedFired=false;
+		public void outputChanged() {
+			this.outputChangedFired=true;
+		}
+	}
+	
+	@Test
+	public void testStackInstructionsChanged() {
+		StackProgram program = new StackProgram();
+		StackMachine machine = new StackMachine(program);
+		MockListener l = new MockListener();
+		machine.addListener(l);
+		machine.addInstruction(0, new Instruction("load", new IntStackValue(2)));
+		assertEquals(true, l.stackInstructionsChangedFired);
+	}
+	
+	@Test 
+	public void testStackProgramCounterChanged(){
+		
+	}
+	
+	@Test 
+	public void testStoreChanged(){
+		
+	}
+	
+	@Test 
+	public void testInputConsumed(){
+		
+	}
+	
+	@Test 
+	public void testOutputChanged(){
+		
+	}
+}
