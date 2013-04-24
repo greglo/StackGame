a68e63f579cf937fc81265439edb391200cd041a
Michael Savage <mikejsavage@gmail.com>
2013-04-24 20:06:03 +0100

    fix indentation in Instruction
    inflate lines touched stat

--- src/ox/stackgame/stackmachine/instructions/Instruction.java
+++ src/ox/stackgame/stackmachine/instructions/Instruction.java
@@ -9,28 +9,28 @@ public class Instruction {
     public final List<StackValue<?>> args;
 
     public Instruction(String name) {
-	this(name, new ArrayList<StackValue<?>>());
+        this(name, new ArrayList<StackValue<?>>());
     }
-    
-    public Instruction(String name, StackValue<?>... args) {
-	List<StackValue<?>> argsList = new ArrayList<StackValue<?>>();
-	for (StackValue<?> value : args)
-	    argsList.add(value);
 
-	assert name != null : "name == null";
-	assert Operations.get(name) != null : name + " isnt a real instruction";
-	assert argsList != null : "args == null";
-	
-	this.name = name;
-	this.args = argsList;
+    public Instruction(String name, StackValue<?>... args) {
+        List<StackValue<?>> argsList = new ArrayList<StackValue<?>>();
+        for (StackValue<?> value : args)
+            argsList.add(value);
+
+        assert name != null : "name == null";
+        assert Operations.get(name) != null : name + " isnt a real instruction";
+        assert argsList != null : "args == null";
+
+        this.name = name;
+        this.args = argsList;
     }
-    
+
     public Instruction(String name, List<StackValue<?>> args) {
-	assert name != null : "name == null";
-	assert Operations.get(name) != null : name + " isnt a real instruction";
-	assert args != null : "args == null";
-	
-	this.name = name;
-	this.args = args;
+        assert name != null : "name == null";
+        assert Operations.get(name) != null : name + " isnt a real instruction";
+        assert args != null : "args == null";
+
+        this.name = name;
+        this.args = args;
     }
 }
