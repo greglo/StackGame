3e488363dc66e8274f90457e2b82531b3300b6ba
Michael Savage <mikejsavage@gmail.com>
2013-04-26 13:40:58 +0100

    fix labels generation in StackProgram

--- src/ox/stackgame/stackmachine/StackProgram.java
+++ src/ox/stackgame/stackmachine/StackProgram.java
@@ -10,47 +10,47 @@ import ox.stackgame.stackmachine.instructions.*;
 public class StackProgram {
     private final List<Instruction> instructions;
     private final Map<String, Integer> labels;
-    
+
     public StackProgram() {
-	instructions = new ArrayList<Instruction>();
-	labels = new HashMap<String, Integer>();
+        instructions = new ArrayList<Instruction>();
+        labels = new HashMap<String, Integer>();
     }
-    
+
     public StackProgram(String source) {
-	this();
+        this();
     }
-    
+
     public StackProgram(List<Instruction> instructions) {
-	this.instructions = instructions;
-	labels = new HashMap<String, Integer>();
+        this.instructions = instructions;
+        labels = new HashMap<String, Integer>();
 
-	int i = 0;
-	for( Instruction op : instructions ) {
-		if( op.name == "label" ) {
-			labels.put( ( String ) op.args.get( 0 ).getValue(), i );
-		}
+        int i = 0;
+        for( Instruction op : instructions ) {
+            if( op.name.equals( "label" ) ) {
+                labels.put( ( String ) op.arg.getValue(), i );
+            }
 
-		i++;
-	}
+            i++;
+        }
     }
-    
+
     public List<Instruction> getInstructions() {
-	return instructions;
+        return instructions;
     }
-    
+
     public int countInstructions() {
-	return instructions.size();
+        return instructions.size();
     }
-    
+
     public Instruction instructionAt(int index) {
-	return instructions.get(index);
+        return instructions.get(index);
     }
-    
+
     public int getLabelPosition(String identifier) {
-	if (labels.containsKey(identifier))
-	    return labels.get(identifier);
-	else
-	    return -1;
+        if (labels.containsKey(identifier))
+            return labels.get(identifier);
+        else
+            return -1;
     }
-    
+
 }
