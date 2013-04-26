0cf22f53ac48152d76622ffec0e2c536413db8d8
Greg O'Connor <gregory@oconnor.name>
2013-04-26 16:34:01 +0100

    Fill StackMachineListenerTest.java test cases

--- test/ox/stackgame/stackmachine/StackMachineListenerTest.java
+++ test/ox/stackgame/stackmachine/StackMachineListenerTest.java
@@ -1,69 +1,103 @@
 package ox.stackgame.stackmachine;
 
+import static org.junit.Assert.*;
 import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Test;
 
+import ox.stackgame.stackmachine.exceptions.NotHaltingException;
+import ox.stackgame.stackmachine.exceptions.StackRuntimeException;
 import ox.stackgame.stackmachine.instructions.Instruction;
 
 public class StackMachineListenerTest {
 
-	class MockListener implements StackMachineListener{
-		public boolean stackInstructionsChangedFired = false;
-		public void stackInstructionsChanged(List<Instruction> instructions) {
-			this.stackInstructionsChangedFired = true;
-		}
+    class MockListener implements StackMachineListener {
+	public boolean stackInstructionsChangedFired = false;
+	public boolean stackProgramCounterChanged = false;
+	public boolean storeChangedFired = false;
+	public boolean inputConsumedFired = false;
+	public boolean outputChangedFired = false;
 
-		public boolean stackProgramCounterChanged = false;
-		public void programCounterChanged(int line) {
-			this.stackProgramCounterChanged = true;
-		}
+	public void stackInstructionsChanged(List<Instruction> instructions) {
+	    this.stackInstructionsChangedFired = true;
+	}
 
-		public boolean storeChangedFired = false;
-		public void storeChanged(int address) {
-			this.storeChangedFired=true;
-		}
+	public void programCounterChanged(int line) {
+	    this.stackProgramCounterChanged = true;
+	}
 
-		public boolean inputConsumedFired=false;
-		public void inputConsumed(int startIndex) {
-			this.inputConsumedFired=true;
-		}
+	public void storeChanged(int address) {
+	    this.storeChangedFired = true;
+	}
 
-		public boolean outputChangedFired=false;
-		public void outputChanged() {
-			this.outputChangedFired=true;
-		}
+	public void inputConsumed(int startIndex) {
+	    this.inputConsumedFired = true;
 	}
-	
-	@Test
-	public void testStackInstructionsChanged() {
-		StackProgram program = new StackProgram();
-		StackMachine machine = new StackMachine(program);
-		MockListener l = new MockListener();
-		machine.addListener(l);
-		machine.addInstruction(0, new Instruction("load", new IntStackValue(2)));
-		assertEquals(true, l.stackInstructionsChangedFired);
+
+	public void outputChanged() {
+	    this.outputChangedFired = true;
 	}
-	
-	@Test 
-	public void testStackProgramCounterChanged(){
-		
-	}
-	
-	@Test 
-	public void testStoreChanged(){
-		
-	}
-	
-	@Test 
-	public void testInputConsumed(){
-		
-	}
-	
-	@Test 
-	public void testOutputChanged(){
-		
-	}
+    }
+
+    @Test
+    public void testStackInstructionsChanged() throws StackRuntimeException, NotHaltingException {
+	StackProgram program = new StackProgram();
+	StackMachine machine = new StackMachine(program);
+	MockListener l = new MockListener();
+	machine.addListener(l);
+	machine.addInstruction(0, new Instruction("load", new IntStackValue(2)));
+	machine.runAll();
+	assertTrue(l.stackInstructionsChangedFired);
+    }
+
+    @Test
+    public void testStackProgramCounterChanged() throws StackRuntimeException, NotHaltingException {
+	List<Instruction> instructions = new ArrayList<Instruction>();
+	StackMachine machine = new StackMachine(instructions);
+	MockListener l = new MockListener();
+	machine.addListener(l);
+	machine.addInstruction(0, new Instruction("const", new IntStackValue(2)));
+	machine.runAll();
+	assertTrue(l.stackProgramCounterChanged);
+    }
+
+    @Test
+    public void testStoreChanged() throws StackRuntimeException, NotHaltingException {
+	List<Instruction> instructions = new ArrayList<Instruction>();
+	StackMachine machine = new StackMachine(instructions);
+	MockListener l = new MockListener();
+	machine.addListener(l);
+	machine.addInstruction(0, new Instruction("const", new IntStackValue(2)));
+	machine.addInstruction(1, new Instruction("store", new IntStackValue(0)));
+	machine.runAll();
+	assertTrue(l.storeChangedFired);
+    }
+
+    @Test
+    public void testInputConsumed() throws StackRuntimeException, NotHaltingException {
+	List<Instruction> instructions = new ArrayList<Instruction>();
+	List<StackValue<?>> input = new ArrayList<StackValue<?>>();
+	input.add(new IntStackValue(2));
+	StackMachine machine = new StackMachine(instructions, input);
+	MockListener l = new MockListener();
+	machine.addListener(l);
+	machine.addInstruction(0, new Instruction("input"));
+	machine.runAll();
+	assertTrue(l.inputConsumedFired);
+    }
+
+    @Test
+    public void testOutputChanged() throws StackRuntimeException, NotHaltingException {
+	List<Instruction> instructions = new ArrayList<Instruction>();
+	StackMachine machine = new StackMachine(instructions);
+	MockListener l = new MockListener();
+	machine.addListener(l);
+	machine.addInstruction(0, new Instruction("const", new IntStackValue(2)));
+	machine.addInstruction(1, new Instruction("output"));
+	machine.runAll();
+	assertTrue(l.outputChangedFired);
+    }
 }
