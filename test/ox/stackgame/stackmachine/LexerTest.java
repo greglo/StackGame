281c7ce80c2915861b413c3f723eb9c0d5e51cc7
Greg O'Connor <gregory@oconnor.name>
2013-04-26 18:20:53 +0100

    Add more lexer tests

--- test/ox/stackgame/stackmachine/LexerTest.java
+++ test/ox/stackgame/stackmachine/LexerTest.java
@@ -12,56 +12,63 @@ import ox.stackgame.stackmachine.exceptions.StackRuntimeException;
 import ox.stackgame.stackmachine.instructions.Instruction;
 
 public class LexerTest {
-	
 
     @Test(expected = LexerException.class)
-    public void lexerExceptionTest() throws LexerException{
-    	// 'const 5' is legal; 'const' is not.  should throw an exception.
-    	List<Instruction> ops = Lexer.lex("const\n");
-	}
-    
-    @Test(expected = LexerException.class)
-    public void tooManyArgsTest() throws LexerException {
-	List<Instruction> ops = Lexer.lex("const 1 2");
-	System.out.println(ops.get(0).arg.getValue());
+    public void noArgsTest() throws LexerException {
+	// Throws error as no argument provided to 'const'
+	Lexer.lex("const");
     }
 
-	@Test
-	public void lexertest() throws LexerException {
-		List<Instruction> ops = Lexer.lex("jump lab\n" + "const 5\n"
-				+ "label lab\n" + "const 7\n" + "const 100\n" + "add\n"
-				+ "nop\n");
+    @Test(expected = LexerException.class)
+    public void tooManyArgsTest() throws LexerException {
+	// Throws error as too many arguments provided
+	Lexer.lex("const 1 2");
+    }
+    
+    @Test
+    public void formattingTest() throws LexerException {
+	// The lexer should be able to handle weirdly (but validly) formatted code
+	Lexer.lex(" add ");
+	Lexer.lex("ADD");
+	Lexer.lex("const       5");
+    }
 
-		StackMachine m = new StackMachine(ops);
+    @Test
+    public void lexertest() throws LexerException {
+	List<Instruction> ops = Lexer.lex("jump lab\n" + "const 5\n"
+		+ "label lab\n" + "const 7\n" + "const 100\n" + "add\n"
+		+ "nop\n");
 
-		try {
-			m.step();
-			m.step();
-			m.step();
-			m.step();
-			m.step();
-			m.step();
-			m.step();
-			m.step();
-			m.step();
-			assertEquals(107, m.getStack().peek().getValue()); // checks the
-																// value on the
-																// stack is 107
-		} catch (StackRuntimeException e) {
-			System.out.println("it all blew up: " + e.toString());
-		}
+	StackMachine m = new StackMachine(ops);
 
-		try {
-			m.reset();
-			assertEquals(0, m.getProgramCounter());
-			m.runAll();
-			assertEquals(107, m.getStack().peek().getValue());
-		} catch (StackRuntimeException e) {
-			e.printStackTrace();
-		} catch (NotHaltingException e) {
-			e.printStackTrace();
-		}
-
+	try {
+	    m.step();
+	    m.step();
+	    m.step();
+	    m.step();
+	    m.step();
+	    m.step();
+	    m.step();
+	    m.step();
+	    m.step();
+	    assertEquals(107, m.getStack().peek().getValue()); // checks the
+							       // value on the
+							       // stack is 107
+	} catch (StackRuntimeException e) {
+	    System.out.println("it all blew up: " + e.toString());
 	}
+
+	try {
+	    m.reset();
+	    assertEquals(0, m.getProgramCounter());
+	    m.runAll();
+	    assertEquals(107, m.getStack().peek().getValue());
+	} catch (StackRuntimeException e) {
+	    e.printStackTrace();
+	} catch (NotHaltingException e) {
+	    e.printStackTrace();
+	}
+
+    }
 
 }
