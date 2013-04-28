9356c92282c4059993316ef5061c888bfa80b1ed
Ryan <ryan.gossiaux@merton.ox.ac.uk>
2013-04-28 12:18:19 +0100

    ChallengeUI is now only visible in challenge mode

--- src/ox/stackgame/ui/ChallengeUI.java
+++ src/ox/stackgame/ui/ChallengeUI.java
@@ -25,74 +25,77 @@ import ox.stackgame.stackmachine.StackMachineListenerAdapter;
  */
 public class ChallengeUI extends JPanel {
 
-	protected AbstractChallenge challenge;
-	protected JLabel descLabel; 
-	private StackMachineListener l = new StackMachineListenerAdapter() {
-		public void programCounterChanged(int line) {
-			// TODO Check execution hasn't exceeded allowedInstructions
-			// TODO evaluate machine against challenge's hasSucceeded() function
-		}
-
-		// TODO check the stacksize hasn't exceeded challenge's specified
-		// stackSize.
-	};
-
-	public ChallengeUI(StateManager m) {
-
-		// TODO connect up the listener appropriately
-
-		// TODO decide how mode switching should work. (Challenge must be
-		// selected first!)
-		// TODO what happens to this when it's not in challenge mode?
-
-		m.registerModeActivationVisitor(modeActivationVisitor);
-		m.registerModeDeactivationVisitor(modeDeactivationVisitor);
-
-		this.setLayout(new FlowLayout());
-		this.setBackground(Color.white);
-		this.setSize(new Dimension(250, ApplicationFrame.h));
-		this.setBorder(new EmptyBorder(15, 15, 15, 15));
-		JLabel l = new JLabel("Challenge");
-		l.setFont(new Font("Helvetica Neue", Font.BOLD, 23));
-		l.setForeground(new Color(66, 66, 66));
-		this.add(l);
-		descLabel = new JLabel();
-		this.add(descLabel);
-	}
-
-	private ModeVisitor modeActivationVisitor = new ModeVisitor() {
-
-		public void visit(RunMode m) {
-		}
-
-		public void visit(ChallengeMode m) {
-			challenge = m.challenge;
-			descLabel.setText(challenge.description);
-		}
-
-        @Override
-        public void visit(FreeDesignMode m) {
-            // TODO Auto-generated method stub
-            
+    protected AbstractChallenge challenge;
+    protected JLabel descLabel;
+    private StackMachineListener l = new StackMachineListenerAdapter() {
+        public void programCounterChanged(int line) {
+            // TODO Check execution hasn't exceeded allowedInstructions
+            // TODO evaluate machine against challenge's hasSucceeded() function
         }
-	};
 
-	// code to be executed when a mode is deactivated
-	private ModeVisitor modeDeactivationVisitor = new ModeVisitor() {
-		public void visit(RunMode m) {
-		}
+        // TODO check the stacksize hasn't exceeded challenge's specified
+        // stackSize.
+    };
 
-        @Override
+    public ChallengeUI(StateManager m) {
+        m.stackMachine.addListener(l);
+
+        // TODO decide how mode switching should work. (Challenge must be
+        // selected first!)
+        // TODO what happens to this when it's not in challenge mode?
+
+        m.registerModeActivationVisitor(modeActivationVisitor);
+        m.registerModeDeactivationVisitor(modeDeactivationVisitor);
+
+        this.setLayout(new FlowLayout());
+        this.setBackground(Color.white);
+        this.setSize(new Dimension(250, ApplicationFrame.h));
+        this.setBorder(new EmptyBorder(15, 15, 15, 15));
+        JLabel l = new JLabel("Challenge");
+        l.setFont(new Font("Helvetica Neue", Font.BOLD, 23));
+        l.setForeground(new Color(66, 66, 66));
+        this.add(l);
+        descLabel = new JLabel();
+        this.add(descLabel);
+        disableMode();
+    }
+
+    private void disableMode() {
+        // TODO: deal with switching out of challenge mode (unregister listener, etc)
+        this.setVisible(false);
+    }
+    
+    private void enableMode() {
+        // TODO: deal with switching into challenge mode (register listener, update display, etc)
+        this.setVisible(true);
+    }
+
+    private ModeVisitor modeActivationVisitor = new ModeVisitor() {
+
+        public void visit(RunMode m) {
+        }
+
         public void visit(ChallengeMode m) {
-            // TODO Auto-generated method stub
-            
+            challenge = m.challenge;
+            descLabel.setText(challenge.description);
+            enableMode();
         }
 
-        @Override
         public void visit(FreeDesignMode m) {
-            // TODO Auto-generated method stub
-            
         }
-	};
+    };
+
+    // code to be executed when a mode is deactivated
+    private ModeVisitor modeDeactivationVisitor = new ModeVisitor() {
+        public void visit(RunMode m) {
+        }
+
+        public void visit(ChallengeMode m) {
+            disableMode();
+        }
+
+        public void visit(FreeDesignMode m) {
+        }
+    };
 
 }
