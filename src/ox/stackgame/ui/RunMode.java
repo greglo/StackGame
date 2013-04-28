2c9479592b30bdcbb11a5b0909dab8f0ff4cc8ec
Ryan <ryan.gossiaux@merton.ox.ac.uk>
2013-04-28 12:57:58 +0100

    Modify RunMode to store if we're paused or running

--- src/ox/stackgame/ui/RunMode.java
+++ src/ox/stackgame/ui/RunMode.java
@@ -4,9 +4,26 @@
 package ox.stackgame.ui;
 
 /**
+ * 
+ * A new RunMode should be created when we begin running. It stores whether execution has been paused or not.
+ * 
  * @author danfox
+ * @author rgossiaux
  */
+
+// TODO: Think about what this class should do.
+
 public class RunMode extends Mode {
+    
+    protected boolean isRunning = false;
+    
+    public void pause() {
+        isRunning = false;
+    }
+    
+    public void run() {
+        isRunning = true;
+    }
 
     public void accept(ModeVisitor v) {
         v.visit(this);
