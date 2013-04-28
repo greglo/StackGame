e52548fb624a070990b718d380d3bd8a2fed79e4
Dan Fox <iamdanfox@gmail.com>
2013-04-28 15:08:05 +0100

    Slightly better button logic

--- src/ox/stackgame/ui/ButtonUI.java
+++ src/ox/stackgame/ui/ButtonUI.java
@@ -10,12 +10,17 @@ import javax.swing.JButton;
 import javax.swing.JPanel;
 
 import ox.stackgame.stackmachine.StackMachine;
+import ox.stackgame.stackmachine.StackMachineListenerAdapter;
+import ox.stackgame.stackmachine.StackMachine.EvaluationStack;
+import ox.stackgame.stackmachine.StackMachineListener;
 import ox.stackgame.stackmachine.exceptions.StackRuntimeException;
 import ox.stackgame.stackmachine.instructions.Instruction;
 
-public class ButtonUI extends JPanel {
+public class ButtonUI extends JPanel {   
+    
+    
     public ButtonUI(final StateManager modeManager, final ProgramTextUI tui, final RunMode runMode){
-
+        
         this.setBackground(Color.BLACK);
         this.setSize(new Dimension(80,300));
         
@@ -32,6 +37,36 @@ public class ButtonUI extends JPanel {
         final JButton step1Button = new JButton("Step1");
         step1Button.setForeground(new Color(0, 133, 200));
         step1Button.setSize(r, r);
+        this.add(step1Button);
+
+        // create stepAll Button
+        // allows the user to see each command animated until the machine halts.
+        final JButton stepAllButton = new JButton("StepAll");
+        stepAllButton.setForeground(new Color(0, 133, 200));
+        stepAllButton.setSize(r, r);
+        this.add(stepAllButton);
+
+        // pauseButton
+        // only relevant to stepAll Button
+        final JButton pauseButton = new JButton("Pause");
+        pauseButton.setForeground(new Color(0, 133, 200));
+        pauseButton.setEnabled(false);
+        pauseButton.setSize(r, r);
+        this.add(pauseButton);
+
+        // create runAll button
+        final JButton runAllButton = new JButton("RunAll");
+        runAllButton.setForeground(new Color(0, 133, 200));
+        runAllButton.setSize(r, r);
+        this.add(runAllButton);
+
+        // create reset button
+        final JButton resetButton = new JButton("Reset");
+        resetButton.setEnabled(false);
+        resetButton.setSize(r, r);
+        this.add(resetButton);
+
+        // button logic
         step1Button.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent arg0) {
                 // when not in runMode already, feed the text through the lexer,
@@ -54,13 +89,28 @@ public class ButtonUI extends JPanel {
                 // should become disabled when pc is past the end of the program.
             }
         });
-        this.add(step1Button);
-
-        // create stepAll Button
-        // allows the user to see each command animated until the machine halts.
-        final JButton stepAllButton = new JButton("StepAll");
-        stepAllButton.setForeground(new Color(0, 133, 200));
-        stepAllButton.setSize(r, r);
+        
+        runAllButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent arg0) {
+                // for a dirty textarea, lex the text, switch to RunMode
+                // (storing the current mode), call runAll on the stackMachine.
+                // when clicked in RunMode, call runAll
+                // disable this button
+            }
+        });
+        
+        pauseButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent arg0) {
+                // disabled in designMode.
+                // enabled if machine is being animated
+                // when clicked, stop the stepAll timer from calling step
+                // enable stepAllButton, enable runAllButton
+                
+                runMode.pause();
+                pauseButton.setEnabled(false); // cant repause
+            }
+        });
+        
         stepAllButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent arg0) {
                 // for a dirty textarea, lex the text, switch to RunMode
@@ -71,50 +121,32 @@ public class ButtonUI extends JPanel {
                 // enable pausebutton
                 modeManager.setActiveMode(runMode);
                 runMode.run();
+                
+                pauseButton.setEnabled(true);
             }
         });
-        this.add(stepAllButton);
-
-        // pauseButton
-        // only relevant to stepAll Button
-        final JButton pauseButton = new JButton("Pause");
-        pauseButton.setEnabled(false);
-        pauseButton.setForeground(new Color(0, 133, 200));
-        pauseButton.setSize(r, r);
-        pauseButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent arg0) {
-                // disabled in designMode.
-                // enabled if machine is being animated
-                // when clicked, stop the stepAll timer from calling step
-                // enable stepAllButton, enable runAllButton
-            }
-        });
-        this.add(pauseButton);
-
-        // create runAll button
-        final JButton runAllButton = new JButton("RunAll");
-        runAllButton.setForeground(new Color(0, 133, 200));
-        runAllButton.setSize(r, r);
-        runAllButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent arg0) {
-                // for a dirty textarea, lex the text, switch to RunMode
-                // (storing the current mode), call runAll on the stackMachine.
-                // when clicked in RunMode, call runAll
-                // disable this button
-            }
-        });
-        this.add(runAllButton);
-
-        // create reset button
-        final JButton resetButton = new JButton("Reset");
-        resetButton.setEnabled(false);
-        resetButton.setSize(r, r);
+        
         resetButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent arg0) {
                 // (should not be enabled in DesignMode)
                 // switch back to old mode, call reset on the stackmachine
+                
             }
         });
-        this.add(resetButton);
+        
+        // do button enabling/disabling
+        modeManager.stackMachine.addListener( new StackMachineListenerAdapter() {
+            public void programCounterChanged(int line) {
+                // isRunning == true if there are more exceptions to execute
+                Boolean b = modeManager.stackMachine.isRunning();
+                step1Button.setEnabled(b);
+                stepAllButton.setEnabled(b);
+                runAllButton.setEnabled(b);
+                
+                resetButton.setEnabled(modeManager.stackMachine.getProgramCounter() != 0);
+                
+            }
+        });
+        
     }
 }
