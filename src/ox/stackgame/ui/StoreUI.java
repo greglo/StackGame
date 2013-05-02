e5f2fc56a006eef8a44dfc263f0f883fd45ef40b
Greg O'Connor <gregory@oconnor.name>
2013-05-02 14:29:34 +0100

    Changes t StoreUI.java. Added resources/ source folder

--- src/ox/stackgame/ui/StoreUI.java
+++ src/ox/stackgame/ui/StoreUI.java
@@ -10,6 +10,7 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.geom.Rectangle2D;
 
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
@@ -29,19 +30,21 @@ import ox.stackgame.stackmachine.exceptions.InvalidAddressException;
 public class StoreUI extends JPanel {
     private JLabel label;
     private final StackMachine activeMachine;
-    private static int BOX_WIDTH = 80;
-    private static int BOX_HEIGHT = 30;
-    private static Color EVEN_COLOR = ApplicationFrame.caBlueL;
-    private static Color ODD_COLOR = ApplicationFrame.caBlue2L;
+    private static int CONTROL_WIDTH    = 90;
+    private static int TEXT_HEIGHT      = 20;
+    private static int RAM_WIDTH        = 80;
+    private static int RAM_HEIGHT       = 45;
+    private static int Y_PADDING        = 15;
+
     
     public StoreUI(StateManager m) {
         activeMachine = m.stackMachine;
         activeMachine.addListener(l);
         
         label = new JLabel("Store:", SwingConstants.CENTER);
-        label.setBounds(0, 0, BOX_WIDTH, 25);
-        this.setBackground(ApplicationFrame.caBlue2L);
-        this.setSize(new Dimension(BOX_WIDTH, 25 + BOX_HEIGHT * StackMachine.STORE_SIZE));        
+        label.setBounds(0, 0, CONTROL_WIDTH, 25);
+        this.setBackground(ApplicationFrame.caBlueL);
+        this.setSize(new Dimension(CONTROL_WIDTH, 25 + (RAM_HEIGHT + TEXT_HEIGHT + Y_PADDING) * StackMachine.STORE_SIZE));        
         this.add(label);
     }
 
@@ -56,22 +59,30 @@ public class StoreUI extends JPanel {
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D g2d = (Graphics2D) g;
-        int yOffset = 25;
-        g2d.setFont(new Font(g2d.getFont().getName(), Font.PLAIN, 20));
+        int yOffset = 25 + Y_PADDING / 2;
         
         try {
             for (int i = 0; i < activeMachine.STORE_SIZE; i++) {
-                g2d.setColor((i % 2 == 0) ? EVEN_COLOR : ODD_COLOR);
-                g2d.fillRect(0, yOffset, BOX_WIDTH, BOX_HEIGHT);
+                g2d.setColor(new Color(0,0,0));
+                g2d.setFont(new Font(g2d.getFont().getName(), Font.PLAIN, 10));
+                String addressString = "0x" + i;
+                Rectangle2D addressBox = g2d.getFontMetrics().getStringBounds(addressString, g2d);
+                int addressX = (CONTROL_WIDTH - (int)addressBox.getWidth()) / 2;
+                int addressY = ((TEXT_HEIGHT - (int)addressBox.getHeight()) / 2);
+                g2d.drawString(addressString, addressX, yOffset + addressY + g2d.getFontMetrics().getAscent());
                 
-                String string = i + ": " + activeMachine.getStore(i).toString();
-                System.out.println(string);
-                Rectangle2D stringBox = g2d.getFontMetrics().getStringBounds(string, g2d);
-                int xStart = (BOX_WIDTH - (int)stringBox.getWidth()) / 2;
-                int yStart = ((BOX_HEIGHT - (int)stringBox.getHeight()) / 2);
-                g2d.setColor((i % 2 == 0) ? ODD_COLOR : EVEN_COLOR);
-                g2d.drawString(string, xStart, yOffset + yStart + g2d.getFontMetrics().getAscent());
-                yOffset += BOX_HEIGHT;
+                ImageIcon icon = new ImageIcon(getClass().getResource("/store.png"));
+                g.drawImage(icon.getImage(), (CONTROL_WIDTH - RAM_WIDTH) / 2, yOffset + TEXT_HEIGHT, RAM_WIDTH, RAM_HEIGHT, null);  
+                
+                g2d.setColor(ApplicationFrame.caBlue2L);
+                g2d.setFont(new Font(g2d.getFont().getName(), Font.PLAIN, 20));
+                String valueString = activeMachine.getStore(i).toString();
+                Rectangle2D valueBox = g2d.getFontMetrics().getStringBounds(valueString, g2d);
+                int valueX = (CONTROL_WIDTH - (int)valueBox.getWidth()) / 2;
+                int valueY = (((RAM_HEIGHT - 5) - (int)valueBox.getHeight()) / 2) + TEXT_HEIGHT;
+                g2d.drawString(valueString, valueX, yOffset + valueY + g2d.getFontMetrics().getAscent());
+                
+                yOffset += TEXT_HEIGHT + RAM_HEIGHT + Y_PADDING;
             }
         }
         catch (InvalidAddressException e) {  }
