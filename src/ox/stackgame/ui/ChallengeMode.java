f0ed2d8ef9f5bdf4629a329fe06b66be315cc89e
Greg O'Connor <gregory@oconnor.name>
2013-05-05 16:26:26 +0100

    Change challenges

--- src/ox/stackgame/ui/ChallengeMode.java
+++ src/ox/stackgame/ui/ChallengeMode.java
@@ -38,22 +38,35 @@ public class ChallengeMode extends DesignMode {
     }
 
     public void setChallenge(AbstractChallenge challenge) {
-        System.out.println("Set current challenge: "+challenge.title);
+        System.out.println("Set current challenge: " + challenge.title);
         this.currChallenge = challenge;
     }
 
     @SuppressWarnings("serial")
     public ChallengeMode() {
         List<AbstractChallenge> cl = challengeList;
+
+        ArrayList<StackValue<?>> tape1to100 = new ArrayList<StackValue<?>>() {
+            {
+                for (int i = 1; i <= 100; i++)
+                    add(new IntStackValue(i));
+            }
+        };
+
+        cl.add(new StackResultChallenge("CONST", 
+                "The CONST instruction <strong>pushes</strong> the given value onto the stack"
+                + "<br/>"
+                + "<br/>Use a CONST instruction to push 1 onto the stack",
+                new HashMap<String, Integer>() {
+                    {
+                        put("const", 1);
+                    }
+                }, new IntStackValue(1)));
         
-        ArrayList<StackValue<?>> tape1to100 = new ArrayList<StackValue<?>>(){{
-            for (int i=1;i<=100;i++) add(new IntStackValue(i));
-        }};
-        
-        // simple Challenge
-        cl.add(new StackResultChallenge(
-                "my first program is balling",
-                "An arithmetic instruction pops two values from the stack, performs some operation and pushes the result back. An example of such an instruction is ADD, which does exactly what you think it does. CONST simply pushes the value after it onto the stack.",
+        cl.add(new StackResultChallenge("Arithmetic Operations", 
+                "Binary arithmetic operations <strong>pop</strong> two values off the stack, and <strong>push</strong> the result of the operation back"
+                + "<br/>"
+                + "<br/>Use the following instructions to get <strong>3</strong> on the stack",
                 new HashMap<String, Integer>() {
                     {
                         put("const 1", 1);
@@ -62,41 +75,19 @@ public class ChallengeMode extends DesignMode {
                     }
                 }, new IntStackValue(3)));
 
-        // simple Challenge
-        try {
-            cl.add(new StackResultChallenge("hey it works with letters too",
-                    "fo reals", new HashMap<String, Integer>() {
-                        {
-                            put("const a", 1); 
-                            put("const 1", 1);
-                            put("add", 1);
-                        }
-                    }, new CharStackValue('b')));
-        } catch (InvalidCharException e) {
-            // initialised it wrong.
-            e.printStackTrace();
-        }
-
-        // simple Challenge
-        cl.add(new StackResultChallenge(
-                "operator precedence is a crutch for bads",
-                "Write a program to compute (4 x 5) - (6 / 2). Notice we are missing a bracket instruction. HMMMMMMMMMMMMMM",
+        cl.add(new StackResultChallenge("Reverse Polish",
+                "There is no way to bracket your programs, so find a way to compute (1 + 2) * 3 = 6 on the stack",
                 new HashMap<String, Integer>() {
                     {
+                        put("const 1", 1);
+                        put("const 2", 1);
                         put("const 3", 1);
-                        put("const 4", 1);
-                        put("const 5", 1);
-                        put("const 6", 1);
+                        put("add", 1);
                         put("mul", 1);
-                        put("sub", 1);
-                        put("div", 1);
                     }
-                }, new IntStackValue(17)));
-        
-        // tape challenge
-        // TODO improve this challenge. it can currently be solved by the empty program...
-        cl.add(new TapeChallenge(
-                "tapes",
+                }, new IntStackValue(6)));
+
+        cl.add(new TapeChallenge("The tape",
                 "Our computer is bleeding edge and has TWO data streams for you to play with input and output. INPUT and OUTPUT read/write one value from/to their respective streams.",
                 new HashMap<String, Integer>() {
                     {
@@ -112,10 +103,9 @@ public class ChallengeMode extends DesignMode {
                         add(new IntStackValue(1));
                     }
                 }));
-        
+
         // why don't we BRANCH OUT?!?!?!
-        cl.add(new TapeChallenge(
-                "why don't we BRANCH OUT?!?!?!",
+        cl.add(new TapeChallenge("why don't we BRANCH OUT?!?!?!",
                 "Today, the input stream contains all the numbers from 1 to 100. Copy it all to output using four instructions.",
                 new HashMap<String, Integer>() {
                     {
@@ -128,21 +118,27 @@ public class ChallengeMode extends DesignMode {
                     {
                         add(new IntStackValue(1));
                     }
-                }, tape1to100)); 
-//        *Obviously if streaming was limited to the last level it would be beyond
-//        worthless, which is why we've helpfully provided an instruction to check if
-//        there's anything left on the input stream. This brings us to the idea of
-//        branching. There are several instructions which will test something about the
-//        machine and make the program jump to a "label" on success, or fall through to
-//        the next instruction if it fails. The one we are introducing here is JII (jump
-//        if input), which will jump to a given label if there is anything left on the
-//        input stream.
-//
-//        label loop
-//        input
-//        output
-//        jii loop
-
+                }, tape1to100));
+        // *Obviously if streaming was limited to the last level it would be
+        // beyond
+        // worthless, which is why we've helpfully provided an instruction to
+        // check if
+        // there's anything left on the input stream. This brings us to the idea
+        // of
+        // branching. There are several instructions which will test something
+        // about the
+        // machine and make the program jump to a "label" on success, or fall
+        // through to
+        // the next instruction if it fails. The one we are introducing here is
+        // JII (jump
+        // if input), which will jump to a given label if there is anything left
+        // on the
+        // input stream.
+        //
+        // label loop
+        // input
+        // output
+        // jii loop
 
         // more branching
         cl.add(new TapeChallenge(
@@ -154,33 +150,36 @@ public class ChallengeMode extends DesignMode {
                         put("const 100", null);
                         put("add", null);
                         put("sub", null);
-                        put("jump *", null);  //PROBLEMS
+                        put("jump *", null); // PROBLEMS
                         put("jez *", null);
                         put("jnez *", null);
                         put("add", null);
                     }
-                }, new ArrayList<StackValue<?>>(), tape1to100));  
-//
-//        ops: const 1, const 100, add, sub, jump, jez, jnez, label, output, dup
-//        in:
-//        out: [1..100]
-//
-//        *JNEZ/JEZ (jump [not] equal zero) are instructions that pop a value off the
-//        stack, compare it with 0 and branch if they are not equal/equal respectively.
-//        JUMP jumps all the time. Output all the numbers from 1 to 100.
-//
-//        Hint: a == b is the same as ( a - b ) == 0*
-//
-//        const 1
-//        label loop
-//        dup
-//        const 100
-//        sub
-//        jez end
-//        output
-//        const 1
-//        add
-//        jump loop
+                }, new ArrayList<StackValue<?>>(), tape1to100));
+        //
+        // ops: const 1, const 100, add, sub, jump, jez, jnez, label, output,
+        // dup
+        // in:
+        // out: [1..100]
+        //
+        // *JNEZ/JEZ (jump [not] equal zero) are instructions that pop a value
+        // off the
+        // stack, compare it with 0 and branch if they are not equal/equal
+        // respectively.
+        // JUMP jumps all the time. Output all the numbers from 1 to 100.
+        //
+        // Hint: a == b is the same as ( a - b ) == 0*
+        //
+        // const 1
+        // label loop
+        // dup
+        // const 100
+        // sub
+        // jez end
+        // output
+        // const 1
+        // add
+        // jump loop
 
         // storegame
         cl.add(new TapeChallenge(
@@ -188,12 +187,12 @@ public class ChallengeMode extends DesignMode {
                 "Being caring and thoughtful the authors have made the extra effort to give users four boxes for storing intermediate results in. LOAD/STORE need to know which box you are accessing and otherwise do exactly what you think they do. Using this, take the pair of numbers from input and output them backwards.",
                 new HashMap<String, Integer>() {
                     {
-                        put("load *", null); 
+                        put("load *", null);
                         put("store *", null);
                         put("input", null);
                         put("output", null);
                     }
-                },  new ArrayList<StackValue<?>>() { 
+                }, new ArrayList<StackValue<?>>() {
                     {
                         add(new IntStackValue(1));
                         add(new IntStackValue(2));
@@ -204,23 +203,27 @@ public class ChallengeMode extends DesignMode {
                         add(new IntStackValue(1));
                     }
                 }));
-//
-//        ops: load, store, input, output
-//        in: 1 2
-//        out: 2 1
-//
-//        Being caring and thoughtful the authors have made the extra effort to give
-//        users four boxes for storing intermediate results in. LOAD/STORE need to know
-//        which box you are accessing and otherwise do exactly what you think they do.
-//        Using this, take the pair of numbers from input and output them backwards.
-//
-//        input
-//        store 1
-//        input
-//        output
-//        load 1
-//        output
-        
+        //
+        // ops: load, store, input, output
+        // in: 1 2
+        // out: 2 1
+        //
+        // Being caring and thoughtful the authors have made the extra effort to
+        // give
+        // users four boxes for storing intermediate results in. LOAD/STORE need
+        // to know
+        // which box you are accessing and otherwise do exactly what you think
+        // they do.
+        // Using this, take the pair of numbers from input and output them
+        // backwards.
+        //
+        // input
+        // store 1
+        // input
+        // output
+        // load 1
+        // output
+
     }
 
 }
